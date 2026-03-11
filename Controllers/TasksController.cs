8ecabda8d9b7561501a15bedc72f971d36a46f4f
STEPS0N <stepan.klimoff@mail.ru>
2026-03-11 21:46:58 +0500

    Реализованный контроллер. Климов

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,59 @@
+﻿using API_Klimov.Context;
+using Microsoft.AspNetCore.Mvc;
+using API_Klimov.Context;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace API_Klimov.Controllers
+{
+    [Route("api/TasksController")]
+    public class TasksController : Controller
+    {
+        ///<summary>
+        ///Получение списка задач
+        ///</summary>
+        ///<remarks>Данный метод получает список задач, находящийся в базе данных</remarks>
+        /// <response code="200">Список успешно получен</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [Route("List")]
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Task>), 200)]
+        [ProducesResponseType(500)]
+
+        public ActionResult List()
+        {
+            try
+            {
+                IEnumerable<Task> Tasks = new TaskContext().Tasks;
+                return Json(Tasks);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        ///<summary>
+        ///Получение задачи
+        ///</summary>
+        ///<remarks>Данный метод получает задачу, находящуюся в базе данных</remarks>
+        /// <response code="200">Задача успешно получена</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [Route("Item")]
+        [HttpGet]
+        [ProducesResponseType(typeof(Task), 200)]
+        [ProducesResponseType(500)]
+
+        public ActionResult Item(int Id)
+        {
+            try
+            {
+                Task Task = new TaskContext().Tasks.Where(x => x.Id == Id).First();
+                return Json(Task);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}

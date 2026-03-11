9f1fca5d05a16ab671a32bc73cc019352b498e71
STEPS0N <stepan.klimoff@mail.ru>
2026-03-11 21:28:31 +0500

    Разработанный контекст для загрузки данных. Климов

---
+++ Context/TaskContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Klimov.Context
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;" +
+                "uid=root;" +
+                "pwd=;" +
+                "database=TaskManager",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}

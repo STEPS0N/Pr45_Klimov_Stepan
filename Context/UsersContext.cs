3645e9d2af8109e8b2db1513e82c175968b773e5
STEPS0N <stepan.klimoff@mail.ru>
2026-03-18 18:38:25 +0500

    Создание модели и контекста пользователя. Климов

---
+++ Context/UsersContext.cs
@@ -0,0 +1,26 @@
+﻿using API_Klimov.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Klimov.Context
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;" +
+                "uid=root;" +
+                "pwd=;" +
+                "port=3307;" +
+                "database=TaskManager",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ Model/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Klimov.Model
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}

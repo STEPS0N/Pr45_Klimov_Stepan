9703f733fa6c6a9e97dda28f1a6075f1f6c217f7
STEPS0N <stepan.klimoff@mail.ru>
2026-03-18 20:09:57 +0500

    Разработанный метод регистрации пользователя. Климов

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -22,7 +22,8 @@ namespace API_Klimov.Controllers
         [ProducesResponseType(typeof(Users), 200)]
         [ProducesResponseType(403)]
         [ProducesResponseType(500)]
-        public ActionResult SignIn([FromForm] string Login, [FromForm]  string Password)
+
+        public ActionResult SignIn([FromForm] string Login, [FromForm] string Password)
         {
             if (Login == null || Password == null)
                 return StatusCode(403);
@@ -32,6 +33,55 @@ namespace API_Klimov.Controllers
                 Users User = new UsersContext().Users.Where(x => x.Login == Login &&
                     x.Password == Password).First();
                 return Json(User);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        /// <summary>
+        /// Регистрация пользователя
+        /// </summary>
+        /// <param name="Login">Логин пользователя</param>
+        /// <param name="Password">Пароль пользователя</param>
+        /// <returns>Данный метод прднозначен для авторизации пользователя на сайте</returns>
+        /// <response code="200">Пользователь успешно зарегистрирован</response>
+        /// <response code="403">Ошибка запроса данные не указаны</response>
+        /// <response code="409">Пользователь с таким логином уже существует</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [Route("RegIn")]
+        [HttpPost]
+        [ProducesResponseType(typeof(Users), 200)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(409)]
+        [ProducesResponseType(500)]
+        public ActionResult RegIn([FromForm] string Login, [FromForm] string Password)
+        {
+            UsersContext usersContext = new UsersContext();
+
+            if (Login == null || Password == null)
+                return StatusCode(403);
+
+            try
+            {
+                var User = usersContext.Users.FirstOrDefault(x => x.Login == Login);
+
+                if (User != null)
+                {
+                    return Conflict("Такой пользователь есть");
+                }
+
+                var newUser = new Users
+                {
+                    Login = Login,
+                    Password = Password
+                };
+
+                usersContext.Users.Add(newUser);
+                usersContext.SaveChanges();
+
+                return Json(newUser);
             }
             catch (Exception ex)
             {
--- Model/Users.cs
+++ Model/Users.cs
@@ -2,8 +2,17 @@
 {
     public class Users
     {
+        /// <summary>
+        /// Код
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Логин
+        /// </summary>
         public string Login { get; set; }
+        /// <summary>
+        /// Пароль
+        /// </summary>
         public string Password { get; set; }
     }
 }
